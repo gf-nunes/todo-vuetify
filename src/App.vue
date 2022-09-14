8b8aed2fe1a75234f793ec9b81531909170a6ccc
gislane.nunes <gislane_nunes@yahoo.com.br>
2022-09-14 16:19:08 -0300

    primeiros passos do todolist

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,88 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+    v-model="drawer"
+      app
+      >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            To-Do
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            List
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>To-Do List</v-toolbar-title>
+    </v-app-bar> -->
+
+        <v-app-bar
+      app
+      color="teal lighten-3"
+      dark
+      src="https://s2.glbimg.com/gR7AqZNmjOBbdN79GNK6OAPzX2c=/940x523/e.glbimg.com/og/ed/f/original/2019/04/18/rotina.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+              items: [
+          { title: 'Tarefas', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Sobre', icon: 'mdi-help-box', to:'/sobre' },
+        ],
+    }),
   }
-}
-</style>
+</script>
